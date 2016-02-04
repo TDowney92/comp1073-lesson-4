de525e8ac2fa5d7c4e9da6d8cb23dd17dad3c683
TDowney92 <tmed425@hotmail.com>
2016-02-04 11:47:09 -0500

    updated app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,24 @@
 
 
 // set your IIFE (Immediatley Invoked Function Expression)
-(function(){
-    
+(function () {
+
     console.log("App Started...");
     
+    
+    //declared a named function way
+    
+    function replaceFirstParagraph() {
+        console.log("inside replaceFirstParagraph function");
+        var firstParagraph;
+
+        firstParagraph = document.getElementById("firstParagraph");
+
+        firstParagraph.innerHTML = "My New Paragraph Data";
+
+
+    }
+    // call the replaceFirstParagraph function
+    replaceFirstParagraph();
+
 })();
